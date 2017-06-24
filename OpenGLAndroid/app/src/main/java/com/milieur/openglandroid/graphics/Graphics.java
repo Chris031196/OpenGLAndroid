0508a0d5ea5241870e320c5207bcebf815c5ede0
Chris031196 <christian.schlienkamp@gmail.com>
2017-06-24 18:48:45 +0200

    project structure

---
+++ OpenGLAndroid/app/src/main/java/com/milieur/openglandroid/graphics/Graphics.java
@@ -0,0 +1,8 @@
+package com.milieur.openglandroid.graphics;
+
+/**
+ * Created by Chris on 24.06.2017.
+ */
+
+public class Graphics {
+}
---
+++ OpenGLAndroid/app/src/main/java/com/milieur/openglandroid/main/MainActivity.java
@@ -0,0 +1,16 @@
+package com.milieur.openglandroid.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Chris on 24.06.2017.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+       // this.setContentView();
+    }
+}
